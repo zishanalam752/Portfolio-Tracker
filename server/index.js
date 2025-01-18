a9a75e2f49d89550540bf7ea9ab617033b338d7a
zishanalam752 <zishanalam752@gmail.com>
2025-01-18 18:56:11 +0530

    changes in the server code

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import stockRoutes from './routes/stocks.js';
 
-dotenv.config(); // Ensure this is called at the top
+dotenv.config({
+    path: '../.env', // Ensure the correct path to the .env file
+}); 
+
+// Add this log after dotenv.config()
+console.log('Environment Variables:', process.env);
+
+// Check if CONNECTION_URL is loaded correctly
+console.log('Connection String:', process.env.CONNECTION_URL);
 
 const app = express();
-const connectionString = process.env.MONGO_URL;
+const connectionString = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -21,11 +29,10 @@ app.get('/', (req, res) => {
 });
 
 if (!connectionString) {
+    console.log(connectionString)
     console.error('CONNECTION_URL is not defined in the environment variables.');
-    // console.error(error.message);
     process.exit(1);
 }
-
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
